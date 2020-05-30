7b5d598ca7658ab8321af0ef7d87db3769ba6b67
SergeyShynkarenko <serg.shunkarenko@gmail.com>
2020-05-30 23:50:04 +0300

    carousel commit

--- gulpfile.js
+++ gulpfile.js
@@ -44,6 +44,8 @@ gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/mmenu/js/jquery.mmenu.all.min.umd.js',
+		'app/libs/equalHeights/equalheights.js',
+		'app/libs/owl.carousel/owl.carousel.min.js',
 		'app/js/common.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
